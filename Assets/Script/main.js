eccca588e21409ae72dad06585cb1ed76ef87e1a
vishw802161 <vishw802161@gmail.com>
2022-09-08 11:57:43 +0530

    update mobile layout

--- Assets/Script/main.js
+++ Assets/Script/main.js
@@ -1,1 +1,14 @@
-console.log("i'm working");
+const selectElement = (selector) => {
+  const element = document.querySelector(selector);
+  if (element) return element;
+  throw new Error(
+    `something went wrong, make sure that ${selector} exist or is typed correctly.`
+  );
+};
+const toggle_links = selectElement(".links");
+const toggle_Btn = selectElement(".mobile-button");
+
+toggle_Btn.addEventListener("click", () => {
+  toggle_Btn.classList.toggle("active");
+  toggle_links.classList.toggle("active");
+});
